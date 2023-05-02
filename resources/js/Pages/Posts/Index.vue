9430d1d4031268cac8aba48f9bd3dc6be18da9e6
Alex Garrett-Smith <alex@codecourse.com>
2023-05-02 12:19:04 +0100

    Episode 10

---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="max-w-3xl w-full mx-auto py-10 space-y-10">
+        Posts
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostIndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', HomeController::class)->name('home');
-Route::get('/dashboard', DashboardController::class)->name('dashboard');
+// Route::get('/', HomeController::class)->name('home');
+// Route::get('/dashboard', DashboardController::class)->name('dashboard');
+
+Route::get('/', PostIndexController::class);
